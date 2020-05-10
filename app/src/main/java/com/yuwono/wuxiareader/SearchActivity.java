2cd278a4a9580bb83687e040b2b6a27b48bd2393
mcay23 <marcelyuwono23@gmail.com>
2020-05-11 00:15:06 +0700

    fixed minor indexing bug
    fixed: books present in 2 different sites always indexed to the first occurrence in the url list

--- app/src/main/java/com/yuwono/wuxiareader/SearchActivity.java
+++ app/src/main/java/com/yuwono/wuxiareader/SearchActivity.java
@@ -20,12 +20,9 @@ import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -35,6 +32,7 @@ public class SearchActivity extends AppCompatActivity {
     ArrayList<String> urls;
     ArrayAdapter<String> adapter;
     int index_size;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +46,7 @@ public class SearchActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.search_list);
 
         titles = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.titles)));
+
         urls = new ArrayList<String>(Arrays.asList(getResources().getStringArray(R.array.urls)));
 
         index_size = titles.size();
@@ -59,7 +58,32 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = (String) (listView.getItemAtPosition(position));
-                final String url = urls.get(titles.indexOf(item));
+                int target = 0;
+                Log.d("ITEM", urls.get(position));
+                for (int i = 0; i <= position; i++) {
+                    String next = (String) (listView.getItemAtPosition(i));
+                    if (item.equals(next)) {
+                        target++;
+                    }
+                }
+                Log.d("TARGET", Integer.toString(target));
+                int occurrence = 1;
+                int j = 0;
+                Iterator<String> iter = titles.iterator();
+                while (iter.hasNext()) {
+                    if (iter.next().equals(item)) {
+                        if (target == occurrence) {
+                            url = urls.get(j);
+                            Log.d("OCCUR", Integer.toString(j));
+                            Log.d("OCCUR", Integer.toString(occurrence) + " " + Integer.toString(target));
+                            break;
+                        }
+                        Log.d("TARGET NOT EQUALS", "OCCUR");
+                        occurrence++;
+                    }
+                    j++;
+                }
+
                 DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
